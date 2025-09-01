8790d42de51d0dc7ba6151de85302e60c6cfae86
biprendra kumar <your_github_username@users.noreply.github.com>
2025-09-01 09:18:22 +0530

    string code base

---
+++ array/reverse.java
@@ -0,0 +1,12 @@
+package array;
+
+public class reverse {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
